1967e8ee96d5b607e6526d051c43a2c800de12aa
virtuosohy <virtuosohy@outlook.com>
2024-09-13 13:49:50 +0800

    feat：内部无接口样式

---
+++ types/OutSide.ts
@@ -0,0 +1,19 @@
+ export interface HYinfoType {
+	WID: string,
+	HYMC: string,
+	HYKSSJ: string,
+	HYDD: string,
+	ZCR: string
+}
+
+export interface YTrowItem {
+	YTMC: string,
+	HBDW_DISPLAY: string,
+	ZT_DISPLAY: string,
+	ZT: string,
+	HHRY: string,
+}
+
+export interface weather {
+	wendu:string;
+}
